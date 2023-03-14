2a3a88f6fb8f3bde98ee0d9061c8e27b682aec56
IlyaPluh <85877006+IlyaPluh@users.noreply.github.com>
2023-03-14 16:29:41 +0300

    add enterlogin

--- work.js
+++ work.js
@@ -5,10 +5,12 @@ const safari = require('selenium-webdriver/safari');
 const driver = new Builder().forBrowser('safari').setSafariOptions(new safari.Options()).build();
 
 // Navigate to 
-driver.get('http://192.168.1.101:3000').then(function() {
+driver.get('http://192.168.1.156:3000').then(function() {
   setTimeout(function() {}, 5000);
 });
 
 
 // Find the 
-driver.findElement(By.xpath("//input[@placeholder='Enter Email']"))
+driver.findElement(By.xpath("//input[@placeholder='Enter Email']")).sendKeys('admin');
+driver.findElement(By.xpath("//input[@placeholder='Enter Password']")).sendKeys('admin');
+driver.findElement(By.className('authorization__button')).click();
